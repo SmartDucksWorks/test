73bd8a03ac848d18dea7121207f3bd6cf290a61d
SmartDucksWorks <98773785+SmartDucksWorks@users.noreply.github.com>
2025-10-02 17:22:56 -0400

    Change base URL for project site to '/test/'

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// IMPORTANT: Project site base must match repo name with trailing slash
 export default defineConfig({
   plugins: [react()],
-  base: "/", // subdomain serves at root
+  base: "/test/",
 });
