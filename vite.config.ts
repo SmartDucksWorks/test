72da3a8fea7907d6d56c42ee6143e91c83c750ae
SmartDucksWorks <98773785+SmartDucksWorks@users.noreply.github.com>
2025-10-02 17:31:23 -0400

    Update site base in vite.config.ts
    Change project site base to serve at root instead of '/test/'.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// IMPORTANT: Project site base must match repo name with trailing slash
 export default defineConfig({
   plugins: [react()],
-  base: "/test/",
+  base: "/", // <-- subdomain serves at root, not /test/
 });
