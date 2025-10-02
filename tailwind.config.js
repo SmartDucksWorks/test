bbcd7ff0c3392947e348fea542299d908a38af23
SmartDucksWorks <98773785+SmartDucksWorks@users.noreply.github.com>
2025-10-02 16:31:54 -0400

    Format tailwind.config.js for better readability

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,6 @@
-module.exports = { content: ["./index.html","./src/**/*.{ts,tsx}"], theme:{extend:{}}, plugins:[] };
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: { extend: {} },
+  plugins: []
+};
